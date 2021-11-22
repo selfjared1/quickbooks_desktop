ef48b5d2bd087cbd9f795a1cdafbce7d15264ef8
selfjared <self.jared@gmail.com>
2021-11-21 17:50:17 -0700

    created replicate for all queries

---
+++ core/data.py
@@ -0,0 +1,286 @@
+
+available_actions = [
+'ARRefundCreditCardAdd',
+'ARRefundCreditCardQuery',
+'AccountAdd',
+'AccountMod',
+'AccountQuery',
+'AccountTaxLineInfoQuery',
+'AgingReportQuery',
+'BarCodeQuery',
+'BillAdd',
+'BillMod',
+'BillPaymentCheckAdd',
+'BillPaymentCheckMod',
+'BillPaymentCheckQuery',
+'BillPaymentCreditCardAdd',
+'BillPaymentCreditCardQuery',
+'BillQuery',
+'BillToPayQuery',
+'BillingRateAdd',
+'BillingRateQuery',
+'BudgetSummaryReportQuery',
+'BuildAssemblyAdd',
+'BuildAssemblyMod',
+'BuildAssemblyQuery',
+'ChargeAdd',
+'ChargeMod',
+'ChargeQuery',
+'CheckAdd',
+'CheckMod',
+'CheckQuery',
+'ClassAdd',
+'ClassMod',
+'ClassQuery',
+'ClearedStatusMod',
+'CompanyActivityQuery',
+'CompanyQuery',
+'CreditCardChargeAdd',
+'CreditCardChargeMod',
+'CreditCardChargeQuery',
+'CreditCardCreditAdd',
+'CreditCardCreditMod',
+'CreditCardCreditQuery',
+'CreditMemoAdd',
+'CreditMemoMod',
+'CreditMemoQuery',
+'CurrencyAdd',
+'CurrencyMod',
+'CurrencyQuery',
+'CustomDetailReportQuery',
+'CustomSummaryReportQuery',
+'CustomerAdd',
+'CustomerMod',
+'CustomerMsgAdd',
+'CustomerMsgQuery',
+'CustomerQuery',
+'CustomerTypeAdd',
+'CustomerTypeQuery',
+'DataEventRecoveryInfoDel',
+'DataEventRecoveryInfoQuery',
+'DataExtAdd',
+'DataExtDefAdd',
+'DataExtDefDel',
+'DataExtDefMod',
+'DataExtDefQuery',
+'DataExtDel',
+'DataExtMod',
+'DateDrivenTermsAdd',
+'DateDrivenTermsQuery',
+'DepositAdd',
+'DepositMod',
+'DepositQuery',
+'EmployeeAdd',
+'EmployeeMod',
+'EmployeeQuery',
+'EntityQuery',
+'EstimateAdd',
+'EstimateMod',
+'EstimateQuery',
+'Form1099CategoryAccountMappingMod',
+'Form1099CategoryAccountMappingQuery',
+'GeneralDetailReportQuery',
+'GeneralSummaryReportQuery',
+'HostQuery',
+'InventoryAdjustmentAdd',
+'InventoryAdjustmentMod',
+'InventoryAdjustmentQuery',
+'InventorySiteAdd',
+'InventorySiteMod',
+'InventorySiteQuery',
+'InvoiceAdd',
+'InvoiceMod',
+'InvoiceQuery',
+'ItemAssembliesCanBuildQuery',
+'ItemDiscountAdd',
+'ItemDiscountMod',
+'ItemDiscountQuery',
+'ItemFixedAssetAdd',
+'ItemFixedAssetMod',
+'ItemFixedAssetQuery',
+'ItemGroupAdd',
+'ItemGroupMod',
+'ItemGroupQuery',
+'ItemInventoryAdd',
+'ItemInventoryAssemblyAdd',
+'ItemInventoryAssemblyMod',
+'ItemInventoryAssemblyQuery',
+'ItemInventoryMod',
+'ItemInventoryQuery',
+'ItemNonInventoryAdd',
+'ItemNonInventoryMod',
+'ItemNonInventoryQuery',
+'ItemOtherChargeAdd',
+'ItemOtherChargeMod',
+'ItemOtherChargeQuery',
+'ItemPaymentAdd',
+'ItemPaymentMod',
+'ItemPaymentQuery',
+'ItemQuery',
+'ItemReceiptAdd',
+'ItemReceiptMod',
+'ItemReceiptQuery',
+'ItemSalesTaxAdd',
+'ItemSalesTaxGroupAdd',
+'ItemSalesTaxGroupMod',
+'ItemSalesTaxGroupQuery',
+'ItemSalesTaxMod',
+'ItemSalesTaxQuery',
+'ItemServiceAdd',
+'ItemServiceMod',
+'ItemServiceQuery',
+'ItemSitesQuery',
+'ItemSubtotalAdd',
+'ItemSubtotalMod',
+'ItemSubtotalQuery',
+'JobReportQuery',
+'JobTypeAdd',
+'JobTypeQuery',
+'JournalEntryAdd',
+'JournalEntryMod',
+'JournalEntryQuery',
+'LeadAdd',
+'LeadMod',
+'LeadQuery',
+'ListDel',
+'ListDeletedQuery',
+'ListDisplayAdd',
+'ListDisplayMod',
+'ListMerge',
+'OtherNameAdd',
+'OtherNameMod',
+'OtherNameQuery',
+'PaymentMethodAdd',
+'PaymentMethodQuery',
+'PayrollDetailReportQuery',
+'PayrollItemNonWageQuery',
+'PayrollItemWageAdd',
+'PayrollItemWageQuery',
+'PayrollSummaryReportQuery',
+'PreferencesQuery',
+'PriceLevelAdd',
+'PriceLevelMod',
+'PriceLevelQuery',
+'PurchaseOrderAdd',
+'PurchaseOrderMod',
+'PurchaseOrderQuery',
+'ReceivePaymentAdd',
+'ReceivePaymentMod',
+'ReceivePaymentQuery',
+'ReceivePaymentToDepositQuery',
+'SalesOrderAdd',
+'SalesOrderMod',
+'SalesOrderQuery',
+'SalesReceiptAdd',
+'SalesReceiptMod',
+'SalesReceiptQuery',
+'SalesRepAdd',
+'SalesRepMod',
+'SalesRepQuery',
+'SalesTaxCodeAdd',
+'SalesTaxCodeMod',
+'SalesTaxCodeQuery',
+'SalesTaxPayableQuery',
+'SalesTaxPaymentCheckAdd',
+'SalesTaxPaymentCheckMod',
+'SalesTaxPaymentCheckQuery',
+'ShipMethodAdd',
+'ShipMethodQuery',
+'SpecialAccountAdd',
+'SpecialItemAdd',
+'StandardTermsAdd',
+'StandardTermsQuery',
+'TemplateQuery',
+'TermsQuery',
+'TimeReportQuery',
+'TimeTrackingAdd',
+'TimeTrackingMod',
+'TimeTrackingQuery',
+'ToDoAdd',
+'ToDoMod',
+'ToDoQuery',
+'TransactionQuery',
+'TransferAdd',
+'TransferInventoryAdd',
+'TransferInventoryMod',
+'TransferInventoryQuery',
+'TransferMod',
+'TransferQuery',
+'TxnDel',
+'TxnDeletedQuery',
+'TxnDisplayAdd',
+'TxnDisplayMod',
+'TxnVoid',
+'UnitOfMeasureSetAdd',
+'UnitOfMeasureSetQuery',
+'VehicleAdd',
+'VehicleMileageAdd',
+'VehicleMileageQuery',
+'VehicleMod',
+'VehicleQuery',
+'VendorAdd',
+'VendorCreditAdd',
+'VendorCreditMod',
+'VendorCreditQuery',
+'VendorMod',
+'VendorQuery',
+'VendorTypeAdd',
+'VendorTypeQuery',
+'WorkersCompCodeAdd',
+'WorkersCompCodeMod',
+'WorkersCompCodeQuery',
+]
+
+query_actions = ['ARRefundCreditCardQuery', 'AccountQuery', 'AccountTaxLineInfoQuery', 'AgingReportQuery', 'BarCodeQuery', 'BillPaymentCheckQuery', 'BillPaymentCreditCardQuery', 'BillQuery', 'BillToPayQuery', 'BillingRateQuery', 'BudgetSummaryReportQuery', 'BuildAssemblyQuery', 'ChargeQuery', 'CheckQuery', 'ClassQuery', 'CompanyActivityQuery', 'CompanyQuery', 'CreditCardChargeQuery', 'CreditCardCreditQuery', 'CreditMemoQuery', 'CurrencyQuery', 'CustomDetailReportQuery', 'CustomSummaryReportQuery', 'CustomerMsgQuery', 'CustomerQuery', 'CustomerTypeQuery', 'DataEventRecoveryInfoQuery', 'DataExtDefQuery', 'DateDrivenTermsQuery', 'DepositQuery', 'EmployeeQuery', 'EntityQuery', 'EstimateQuery', 'Form1099CategoryAccountMappingQuery', 'GeneralDetailReportQuery', 'GeneralSummaryReportQuery', 'HostQuery', 'InventoryAdjustmentQuery', 'InventorySiteQuery', 'InvoiceQuery', 'ItemAssembliesCanBuildQuery', 'ItemDiscountQuery', 'ItemFixedAssetQuery', 'ItemGroupQuery', 'ItemInventoryAssemblyQuery', 'ItemInventoryQuery', 'ItemNonInventoryQuery', 'ItemOtherChargeQuery', 'ItemPaymentQuery', 'ItemQuery', 'ItemReceiptQuery', 'ItemSalesTaxGroupQuery', 'ItemSalesTaxQuery', 'ItemServiceQuery', 'ItemSitesQuery', 'ItemSubtotalQuery', 'JobReportQuery', 'JobTypeQuery', 'JournalEntryQuery', 'LeadQuery', 'ListDeletedQuery', 'OtherNameQuery', 'PaymentMethodQuery', 'PayrollDetailReportQuery', 'PayrollItemNonWageQuery', 'PayrollItemWageQuery', 'PayrollSummaryReportQuery', 'PreferencesQuery', 'PriceLevelQuery', 'PurchaseOrderQuery', 'ReceivePaymentQuery', 'ReceivePaymentToDepositQuery', 'SalesOrderQuery', 'SalesReceiptQuery', 'SalesRepQuery', 'SalesTaxCodeQuery', 'SalesTaxPayableQuery', 'SalesTaxPaymentCheckQuery', 'ShipMethodQuery', 'StandardTermsQuery', 'TemplateQuery', 'TermsQuery', 'TimeReportQuery', 'TimeTrackingQuery', 'ToDoQuery', 'TransactionQuery', 'TransferInventoryQuery', 'TransferQuery', 'TxnDeletedQuery', 'UnitOfMeasureSetQuery', 'VehicleMileageQuery', 'VehicleQuery', 'VendorCreditQuery', 'VendorQuery', 'VendorTypeQuery', 'WorkersCompCodeQuery']
+mod_actions = ['AccountMod', 'BillMod', 'BillPaymentCheckMod', 'BuildAssemblyMod', 'ChargeMod', 'CheckMod', 'ClassMod', 'ClearedStatusMod', 'CreditCardChargeMod', 'CreditCardCreditMod', 'CreditMemoMod', 'CurrencyMod', 'CustomerMod', 'DataExtDefMod', 'DataExtMod', 'DepositMod', 'EmployeeMod', 'EstimateMod', 'Form1099CategoryAccountMappingMod', 'InventoryAdjustmentMod', 'InventorySiteMod', 'InvoiceMod', 'ItemDiscountMod', 'ItemFixedAssetMod', 'ItemGroupMod', 'ItemInventoryAssemblyMod', 'ItemInventoryMod', 'ItemNonInventoryMod', 'ItemOtherChargeMod', 'ItemPaymentMod', 'ItemReceiptMod', 'ItemSalesTaxGroupMod', 'ItemSalesTaxMod', 'ItemServiceMod', 'ItemSubtotalMod', 'JournalEntryMod', 'LeadMod', 'ListDisplayMod', 'OtherNameMod', 'PriceLevelMod', 'PurchaseOrderMod', 'ReceivePaymentMod', 'SalesOrderMod', 'SalesReceiptMod', 'SalesRepMod', 'SalesTaxCodeMod', 'SalesTaxPaymentCheckMod', 'TimeTrackingMod', 'ToDoMod', 'TransferInventoryMod', 'TransferMod', 'TxnDisplayMod', 'VehicleMod', 'VendorCreditMod', 'VendorMod', 'WorkersCompCodeMod']
+add_actions = ['ARRefundCreditCardAdd', 'AccountAdd', 'BillAdd', 'BillPaymentCheckAdd', 'BillPaymentCreditCardAdd', 'BillingRateAdd', 'BuildAssemblyAdd', 'ChargeAdd', 'CheckAdd', 'ClassAdd', 'CreditCardChargeAdd', 'CreditCardCreditAdd', 'CreditMemoAdd', 'CurrencyAdd', 'CustomerAdd', 'CustomerMsgAdd', 'CustomerTypeAdd', 'DataExtAdd', 'DataExtDefAdd', 'DateDrivenTermsAdd', 'DepositAdd', 'EmployeeAdd', 'EstimateAdd', 'InventoryAdjustmentAdd', 'InventorySiteAdd', 'InvoiceAdd', 'ItemDiscountAdd', 'ItemFixedAssetAdd', 'ItemGroupAdd', 'ItemInventoryAdd', 'ItemInventoryAssemblyAdd', 'ItemNonInventoryAdd', 'ItemOtherChargeAdd', 'ItemPaymentAdd', 'ItemReceiptAdd', 'ItemSalesTaxAdd', 'ItemSalesTaxGroupAdd', 'ItemServiceAdd', 'ItemSubtotalAdd', 'JobTypeAdd', 'JournalEntryAdd', 'LeadAdd', 'ListDisplayAdd', 'OtherNameAdd', 'PaymentMethodAdd', 'PayrollItemWageAdd', 'PriceLevelAdd', 'PurchaseOrderAdd', 'ReceivePaymentAdd', 'SalesOrderAdd', 'SalesReceiptAdd', 'SalesRepAdd', 'SalesTaxCodeAdd', 'SalesTaxPaymentCheckAdd', 'ShipMethodAdd', 'SpecialAccountAdd', 'SpecialItemAdd', 'StandardTermsAdd', 'TimeTrackingAdd', 'ToDoAdd', 'TransferAdd', 'TransferInventoryAdd', 'TxnDisplayAdd', 'UnitOfMeasureSetAdd', 'VehicleAdd', 'VehicleMileageAdd', 'VendorAdd', 'VendorCreditAdd', 'VendorTypeAdd', 'WorkersCompCodeAdd']
+del_actions = ['DataEventRecoveryInfoDel', 'DataExtDefDel', 'DataExtDel', 'ListDel', 'TxnDel']
+
+
+def sort_actions(available_actions):
+    query_end = []
+    mod_end = []
+    add_end = []
+    del_end = []
+    other_end = []
+
+    for action in available_actions:
+        end = action[-3:]
+        if end == 'ery':
+            query_end += [action]
+        elif end == 'Mod':
+            mod_end += [action]
+        elif end == 'Add':
+            add_end += [action]
+        elif end == 'Del':
+            del_end += [action]
+        else:
+            other_end += [action]
+
+    print(query_end)
+    print(mod_end)
+    print(add_end)
+    print(del_end)
+
+def convert_to_python(str_proper):
+    str_proper.replace("Query", "")
+    new_str = ''
+    for i, letter in enumerate(str_proper):
+        if i == 0:
+            new_str += letter.lower()
+        elif i and letter.isupper():
+            new_str += f'_{letter.lower()}'
+        else:
+            new_str += letter
+    return new_str
+
+def code_query(query_actions):
+    for query in query_actions:
+        str_code = "\'"
+        str_code += convert_to_python(query)
+        str_code += f"\':\'{query}\',"
+        print(str_code)
+
+if __name__ == '__main__':
+    # sort_actions(available_actions)
+    code_query(query_actions)
