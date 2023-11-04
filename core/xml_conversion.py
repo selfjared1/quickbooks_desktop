62ab026452131ec6f77bbfd10e924d29f6451453
Jared Self <self.jared@gmail.com>
2023-11-03 22:17:01 -0600

    adding mixins to xml_conversion

--- core/xml_conversion.py
+++ core/xml_conversion.py
@@ -1,71 +1,80 @@
-from xml.etree import cElementTree as ElementTree
+from lxml import etree
 
-class XmlListToObject():
-    def __init__(self, list_of_xml):
-        for element in list_of_xml:
-            for att in element:
-                print(att)
-
-
-
-class XmlListConfig(list):
-    def __init__(self, aList):
-        for element in aList:
-            if element:
-                # treat like dict
-                if len(element) == 1 or element[0].tag != element[1].tag:
-                    self.append(XmlDictConfig(element))
-                # treat like list
-                elif element[0].tag == element[1].tag:
-                    self.append(XmlListConfig(element))
-            elif element.text:
-                text = element.text.strip()
-                if text:
-                    self.append(text)
-
-
-class XmlDictConfig(dict):
-    '''
-    Example usage:
-
-    >>> tree = ElementTree.parse('your_file.xml')
-    >>> root = tree.getroot()
-    >>> xmldict = XmlDictConfig(root)
-
-    Or, if you want to use an XML string:
-
-    >>> root = ElementTree.XML(xml_string)
-    >>> xmldict = XmlDictConfig(root)
-
-    And then use xmldict for what it is... a dict.
-    '''
-    def __init__(self, parent_element):
-        if parent_element.items():
-            self.update(dict(parent_element.items()))
-        for element in parent_element:
-            if element:
-                # treat like dict - we assume that if the first two tags
-                # in a series are different, then they are all different.
-                if len(element) == 1 or element[0].tag != element[1].tag:
-                    aDict = XmlDictConfig(element)
-                # treat like list - we assume that if the first two tags
-                # in a series are the same, then the rest are the same.
-                else:
-                    # here, we put the list in dictionary; the key is the
-                    # tag name the list elements all share in common, and
-                    # the value is the list itself
-                    aDict = {element[0].tag: XmlListConfig(element)}
-                # if the tag has attributes, add those to the dict
-                if element.items():
-                    aDict.update(dict(element.items()))
-                self.update({element.tag: aDict})
-            # this assumes that if you've got an attribute in a tag,
-            # you won't be having any text. This may or may not be a
-            # good idea -- time will tell. It works for the way we are
-            # currently doing XML configuration files...
-            elif element.items():
-                self.update({element.tag: dict(element.items())})
-            # finally, if there are no child tags and no attributes, extract
-            # the text
+class ToXmlMixin(object):
+    def to_xml(self, name_of_start_tag=None):
+        if name_of_start_tag is None:
+            name_of_start_tag = self.__class__.__name__
+        else:
+            name_of_start_tag = name_of_start_tag.replace('_', '')
+        root = etree.Element(name_of_start_tag)
+        for key, value in vars(self).items():
+            if not key.startswith('_') and value is not None:
+                child = etree.SubElement(root, key)
+                child.text = str(value)
             else:
-                self.update({element.tag: element.text})
+                pass
+        return etree.tostring(root, pretty_print=True, encoding='utf-8').decode('utf-8')
+
+class FromXMLMixin(object):
+    class_dict = {}
+    list_dict = {}
+
+    @classmethod
+    def from_xml(cls, xml_data):
+        obj = cls()
+        root = etree.fromstring(xml_data)
+        for key in root.keys():
+            if key in obj.class_dict:
+                sub_obj = obj.class_dict[key]()
+                sub_xml_data = etree.tostring(root.find(key))
+                sub_obj = sub_obj.from_xml(sub_xml_data)
+                setattr(obj, key, sub_obj)
+
+            elif key in obj.list_dict:
+                sub_list = []
+                for elem in root.findall(key):
+                    sub_xml_data = etree.tostring(elem)
+
+                    if 'DetailType' in elem.keys() and elem.get('DetailType') in obj.detail_dict:
+                        sub_obj = obj.detail_dict[elem.get('DetailType')]()
+                    else:
+                        sub_obj = obj.list_dict[key]()
+
+                    sub_obj = sub_obj.from_xml(sub_xml_data)
+                    sub_list.append(sub_obj)
+
+                setattr(obj, key, sub_list)
+            else:
+                setattr(obj, key, root.get(key))
+
+        return obj
+
+
+# Based on http://stackoverflow.com/a/1118038
+def to_dict(obj, classkey=None):
+    """
+    Recursively converts Python object into a dictionary
+    """
+    if isinstance(obj, dict):
+        data = {}
+        for (k, v) in obj.items():
+            data[k] = to_dict(v, classkey)
+        return data
+    elif hasattr(obj, "_ast"):
+        return to_dict(obj._ast())
+    elif hasattr(obj, "__iter__") and not isinstance(obj, str):
+        return [to_dict(v, classkey) for v in obj]
+    elif hasattr(obj, "__dict__"):
+        data = dict([(key, to_dict(value, classkey))
+                    for key, value in obj.__dict__.items()
+                    if not callable(value) and not key.startswith('_')])
+
+        if classkey is not None and hasattr(obj, "__class__"):
+            data[classkey] = obj.__class__.__name__
+        return data
+    else:
+        return obj
+
+class ToDictMixin(object):
+    def to_dict(self):
+        return to_dict(self)
