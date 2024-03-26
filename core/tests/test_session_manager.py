a3e627e29827b02722c2af1536b93cf13bc0a6f0
Jared Self <self.jared@gmail.com>
2024-03-26 13:11:29 -0600

    Update SessionManager import in tests
    The import statement for SessionManager in core/tests/test_session_manager.py has been updated. Previously, it was importing SessionManager from the core.session_manager module, but it now imports from the quickbooks_desktop.session_manager module.

--- core/tests/test_session_manager.py
+++ core/tests/test_session_manager.py
@@ -1,9 +1,8 @@
 import unittest
-<<<<<<<< HEAD:core/tests/test_session_manager.py
 from core.session_manager import SessionManager
-========
+
 from quickbooks_desktop.session_manager import SessionManager
->>>>>>>> develop:src/quickbooks_desktop/tests/test_session_manager.py
+
 from lxml import etree as et
 
 class TestSessionManagerCommon(unittest.TestCase):
