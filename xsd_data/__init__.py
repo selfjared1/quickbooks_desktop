eb81466945ccdc7c34b9e0fe571b5325aaa94f86
Jared Self <self.jared@gmail.com>
2024-09-30 22:49:46 -0600

    Add initialization files for `xsd_data` and root package
    Created `__init__.py` files to facilitate module imports for `xsd_data` and the root package. This includes defining necessary exports for both `qb_so_classes` in `xsd_data` and `my_qb_classes` in the root package.

---
+++ xsd_data/__init__.py
@@ -0,0 +1,21 @@
+from qb_so_classes import (
+    SignonAppCertRqType,
+    SignonAppCertRsType,
+    SignonDesktopRqType,
+    SignonDesktopRsType,
+    SignonMsgsRq,
+    SignonMsgsRs,
+    SignonTicketRqType,
+    SignonTicketRsType,
+)
+
+__all__ = [
+    "SignonAppCertRqType",
+    "SignonAppCertRsType",
+    "SignonDesktopRqType",
+    "SignonDesktopRsType",
+    "SignonMsgsRq",
+    "SignonMsgsRs",
+    "SignonTicketRqType",
+    "SignonTicketRsType",
+]
