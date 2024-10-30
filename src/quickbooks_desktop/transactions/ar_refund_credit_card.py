3f88ec452a883b29a6fa8ee63159ba606c15dd5a
Jared Self <self.jared@gmail.com>
2024-10-30 13:00:34 -0600

    Simplify import statements in ar_refund_credit_card.py
    Removed unused and redundant imports to streamline the `ar_refund_credit_card.py` file. This refactoring improves code readability and reduces potential maintenance overhead.

--- src/quickbooks_desktop/transactions/ar_refund_credit_card.py
+++ src/quickbooks_desktop/transactions/ar_refund_credit_card.py
@@ -1,23 +1,20 @@
 from dataclasses import dataclass, field
 from decimal import Decimal
-from typing import Optional, List, Type, Dict
-from collections import defaultdict
+from typing import Optional, List
 from src.quickbooks_desktop.data_ext import DataExt
 
 from src.quickbooks_desktop.qb_special_fields import QBDates, QBTime
 from src.quickbooks_desktop.common import (
-    LinkedTxn, BillAddress, CreditCardTxnInfo, ShipAddress, ShipAddressBlock, SetCredit,
+    CreditCardTxnInfo,
     ModifiedDateRangeFilter, TxnDateRangeFilter, EntityFilter, AccountFilter, RefNumberFilter, RefNumberRangeFilter,
     CurrencyFilter, Address, RefundAppliedToTxnAdd, AddressBlock, RefundAppliedToTxn
 )
 from src.quickbooks_desktop.lists import (
-    RefundFromAccountRef, PaymentMethodRef, TemplateRef, CustomerRef, ClassInQBRef, AraccountRef, CurrencyRef, TermsRef,
-    SalesRepRef, ShipMethodRef, CustomerMsgRef, CustomerSalesTaxCodeRef, ItemRef, OverrideUomsetRef, InventorySiteRef,
-    InventorySiteLocationRef, ItemGroupRef, PriceLevelRef, OverrideItemAccountRef, AccountRef
+    RefundFromAccountRef, PaymentMethodRef, CustomerRef, AraccountRef, CurrencyRef,
 )
 from src.quickbooks_desktop.mixins import (
-    PluralMixin, PluralTrxnSaveMixin, QBRefMixin, QBMixinWithQuery, QBMixin,
-    QBQueryMixin, QBAddMixin, QBModMixin, SaveMixin
+    PluralMixin, PluralTrxnSaveMixin, QBMixinWithQuery,
+    QBQueryMixin, QBAddMixin,
 )
 
 @dataclass
