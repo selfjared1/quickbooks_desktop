3571f27b356a4be795daf30ebf64db858e7ebf30
Jared Self <self.jared@gmail.com>
2024-10-30 17:20:29 -0600

    Add support for Credit Card Credit transactions
    This commit introduces a new module for handling Credit Card Credit transactions in QuickBooks Desktop. It includes data classes for querying, adding, and modifying transactions, as well as a main class for managing these operations.

---
+++ src/quickbooks_desktop/transactions/credit_card_credit.py
@@ -0,0 +1,503 @@
+from dataclasses import dataclass, field
+from decimal import Decimal
+from typing import Optional, List, Type
+from src.quickbooks_desktop.data_ext import DataExt
+
+from src.quickbooks_desktop.qb_special_fields import QBDates, QBTime
+from src.quickbooks_desktop.common import (
+    ModifiedDateRangeFilter, TxnDateRangeFilter, EntityFilter, AccountFilter, RefNumberFilter, RefNumberRangeFilter,
+    CurrencyFilter, PayeeEntityRef, ExpenseLineAdd,
+    ItemLineAdd, ItemGroupLineAdd, ExpenseLineMod, ItemLineMod, ItemGroupLineMod,
+    ExpenseLine, ItemLine, ItemGroupLine
+)
+from src.quickbooks_desktop.lists import (
+    AccountRef, SalesTaxCodeRef, CurrencyRef
+)
+from src.quickbooks_desktop.mixins import (
+    PluralMixin, PluralTrxnSaveMixin, QBMixinWithQuery,
+    QBQueryMixin, QBAddMixin, QBModMixin
+)
+
+
+@dataclass
+class CreditCardCreditQuery(QBQueryMixin):
+    FIELD_ORDER = [
+        "TxnID", "RefNumber", "RefNumberCaseSensitive", "MaxReturned",
+        "ModifiedDateRangeFilter", "TxnDateRangeFilter", "EntityFilter",
+        "AccountFilter", "RefNumberFilter", "RefNumberRangeFilter",
+        "CurrencyFilter", "IncludeLineItems", "IncludeRetElement", "OwnerID"
+    ]
+
+    class Meta:
+        name = "CreditCardCreditQuery"
+
+    txn_id: List[str] = field(
+        default_factory=list,
+        metadata={
+            "name": "TxnID",
+            "type": "Element",
+        },
+    )
+    ref_number: List[str] = field(
+        default_factory=list,
+        metadata={
+            "name": "RefNumber",
+            "type": "Element",
+        },
+    )
+    ref_number_case_sensitive: List[str] = field(
+        default_factory=list,
+        metadata={
+            "name": "RefNumberCaseSensitive",
+            "type": "Element",
+        },
+    )
+    max_returned: Optional[int] = field(
+        default=None,
+        metadata={
+            "name": "MaxReturned",
+            "type": "Element",
+        },
+    )
+    modified_date_range_filter: Optional[ModifiedDateRangeFilter] = field(
+        default=None,
+        metadata={
+            "name": "ModifiedDateRangeFilter",
+            "type": "Element",
+        },
+    )
+    txn_date_range_filter: Optional[TxnDateRangeFilter] = field(
+        default=None,
+        metadata={
+            "name": "TxnDateRangeFilter",
+            "type": "Element",
+        },
+    )
+    entity_filter: Optional[EntityFilter] = field(
+        default=None,
+        metadata={
+            "name": "EntityFilter",
+            "type": "Element",
+        },
+    )
+    account_filter: Optional[AccountFilter] = field(
+        default=None,
+        metadata={
+            "name": "AccountFilter",
+            "type": "Element",
+        },
+    )
+    ref_number_filter: Optional[RefNumberFilter] = field(
+        default=None,
+        metadata={
+            "name": "RefNumberFilter",
+            "type": "Element",
+        },
+    )
+    ref_number_range_filter: Optional[RefNumberRangeFilter] = field(
+        default=None,
+        metadata={
+            "name": "RefNumberRangeFilter",
+            "type": "Element",
+        },
+    )
+    currency_filter: Optional[CurrencyFilter] = field(
+        default=None,
+        metadata={
+            "name": "CurrencyFilter",
+            "type": "Element",
+        },
+    )
+    include_line_items: Optional[bool] = field(
+        default=None,
+        metadata={
+            "name": "IncludeLineItems",
+            "type": "Element",
+        },
+    )
+    include_ret_element: List[str] = field(
+        default_factory=list,
+        metadata={
+            "name": "IncludeRetElement",
+            "type": "Element",
+            "max_length": 50,
+        },
+    )
+    owner_id: List[str] = field(
+        default_factory=list,
+        metadata={
+            "name": "OwnerID",
+            "type": "Element",
+        },
+    )
+
+
+@dataclass
+class CreditCardCreditAdd(QBAddMixin):
+    FIELD_ORDER = [
+        "AccountRef", "PayeeEntityRef", "TxnDate", "RefNumber", "Memo",
+        "ExchangeRate", "ExternalGUID", "ExpenseLineAdd", "ItemLineAdd",
+        "ItemGroupLineAdd"
+    ]
+
+    class Meta:
+        name = "CreditCardCreditAdd"
+
+    account_ref: Optional[AccountRef] = field(
+        default=None,
+        metadata={
+            "name": "AccountRef",
+            "type": "Element",
+            "required": True,
+        },
+    )
+    payee_entity_ref: Optional[PayeeEntityRef] = field(
+        default=None,
+        metadata={
+            "name": "PayeeEntityRef",
+            "type": "Element",
+        },
+    )
+    txn_date: Optional[QBDates] = field(
+        default=None,
+        metadata={
+            "name": "TxnDate",
+            "type": "Element",
+        },
+    )
+    ref_number: Optional[str] = field(
+        default=None,
+        metadata={
+            "name": "RefNumber",
+            "type": "Element",
+            "max_length": 11,
+        },
+    )
+    memo: Optional[str] = field(
+        default=None,
+        metadata={
+            "name": "Memo",
+            "type": "Element",
+            "max_length": 4095,
+        },
+    )
+    is_tax_included: Optional[bool] = field(
+        default=None,
+        metadata={
+            "name": "IsTaxIncluded",
+            "type": "Element",
+        },
+    )
+    sales_tax_code_ref: Optional[SalesTaxCodeRef] = field(
+        default=None,
+        metadata={
+            "name": "SalesTaxCodeRef",
+            "type": "Element",
+        },
+    )
+    exchange_rate: Optional[float] = field(
+        default=None,
+        metadata={
+            "name": "ExchangeRate",
+            "type": "Element",
+        },
+    )
+    external_guid: Optional[str] = field(
+        default=None,
+        metadata={
+            "name": "ExternalGUID",
+            "type": "Element",
+        },
+    )
+    expense_line_add: List[ExpenseLineAdd] = field(
+        default_factory=list,
+        metadata={
+            "name": "ExpenseLineAdd",
+            "type": "Element",
+        },
+    )
+    item_line_add: List[ItemLineAdd] = field(
+        default_factory=list,
+        metadata={
+            "name": "ItemLineAdd",
+            "type": "Element",
+        },
+    )
+    item_group_line_add: List[ItemGroupLineAdd] = field(
+        default_factory=list,
+        metadata={
+            "name": "ItemGroupLineAdd",
+            "type": "Element",
+        },
+    )
+    def_macro: Optional[str] = field(
+        default=None,
+        metadata={
+            "name": "defMacro",
+            "type": "Attribute",
+        },
+    )
+
+
+@dataclass
+class CreditCardCreditMod(QBModMixin):
+    FIELD_ORDER = [
+        "TxnID", "EditSequence", "AccountRef", "PayeeEntityRef", "TxnDate",
+        "RefNumber", "Memo", "ExchangeRate", "ClearExpenseLines",
+        "ExpenseLineMod", "ClearItemLines", "ItemLineMod", "ItemGroupLineMod"
+    ]
+
+    class Meta:
+        name = "CreditCardCreditMod"
+
+    txn_id: Optional[str] = field(
+        default=None,
+        metadata={
+            "name": "TxnID",
+            "type": "Element",
+            "required": True,
+        },
+    )
+    edit_sequence: Optional[str] = field(
+        default=None,
+        metadata={
+            "name": "EditSequence",
+            "type": "Element",
+            "required": True,
+            "max_length": 16,
+        },
+    )
+    account_ref: Optional[AccountRef] = field(
+        default=None,
+        metadata={
+            "name": "AccountRef",
+            "type": "Element",
+        },
+    )
+    payee_entity_ref: Optional[PayeeEntityRef] = field(
+        default=None,
+        metadata={
+            "name": "PayeeEntityRef",
+            "type": "Element",
+        },
+    )
+    txn_date: Optional[QBDates] = field(
+        default=None,
+        metadata={
+            "name": "TxnDate",
+            "type": "Element",
+        },
+    )
+    ref_number: Optional[str] = field(
+        default=None,
+        metadata={
+            "name": "RefNumber",
+            "type": "Element",
+            "max_length": 11,
+        },
+    )
+    memo: Optional[str] = field(
+        default=None,
+        metadata={
+            "name": "Memo",
+            "type": "Element",
+            "max_length": 4095,
+        },
+    )
+    exchange_rate: Optional[float] = field(
+        default=None,
+        metadata={
+            "name": "ExchangeRate",
+            "type": "Element",
+        },
+    )
+    clear_expense_lines: Optional[bool] = field(
+        default=None,
+        metadata={
+            "name": "ClearExpenseLines",
+            "type": "Element",
+        },
+    )
+    expense_line_mod: List[ExpenseLineMod] = field(
+        default_factory=list,
+        metadata={
+            "name": "ExpenseLineMod",
+            "type": "Element",
+        },
+    )
+    clear_item_lines: Optional[bool] = field(
+        default=None,
+        metadata={
+            "name": "ClearItemLines",
+            "type": "Element",
+        },
+    )
+    item_line_mod: List[ItemLineMod] = field(
+        default_factory=list,
+        metadata={
+            "name": "ItemLineMod",
+            "type": "Element",
+        },
+    )
+    item_group_line_mod: List[ItemGroupLineMod] = field(
+        default_factory=list,
+        metadata={
+            "name": "ItemGroupLineMod",
+            "type": "Element",
+        },
+    )
+    
+
+@dataclass
+class CreditCardCredit(QBMixinWithQuery):
+    
+    class Meta:
+        name = "CurrencyFilter"
+
+    Query: Type[CreditCardCreditQuery] = CreditCardCreditQuery
+    Add: Type[CreditCardCreditAdd] = CreditCardCreditAdd
+    Mod: Type[CreditCardCreditMod] = CreditCardCreditMod
+    
+    txn_id: Optional[str] = field(
+        default=None,
+        metadata={
+            "name": "TxnID",
+            "type": "Element",
+        },
+    )
+    time_created: Optional[QBTime] = field(
+        default=None,
+        metadata={
+            "name": "TimeCreated",
+            "type": "Element",
+        },
+    )
+    time_modified: Optional[QBTime] = field(
+        default=None,
+        metadata={
+            "name": "TimeModified",
+            "type": "Element",
+        },
+    )
+    edit_sequence: Optional[str] = field(
+        default=None,
+        metadata={
+            "name": "EditSequence",
+            "type": "Element",
+            "max_length": 16,
+        },
+    )
+    txn_number: Optional[str] = field(
+        default=None,
+        metadata={
+            "name": "TxnNumber",
+            "type": "Element",
+        },
+    )
+    account_ref: Optional[AccountRef] = field(
+        default=None,
+        metadata={
+            "name": "AccountRef",
+            "type": "Element",
+        },
+    )
+    payee_entity_ref: Optional[PayeeEntityRef] = field(
+        default=None,
+        metadata={
+            "name": "PayeeEntityRef",
+            "type": "Element",
+        },
+    )
+    txn_date: Optional[QBDates] = field(
+        default=None,
+        metadata={
+            "name": "TxnDate",
+            "type": "Element",
+        },
+    )
+    amount: Optional[Decimal] = field(
+        default=None,
+        metadata={
+            "name": "Amount",
+            "type": "Element",
+        },
+    )
+    currency_ref: Optional[CurrencyRef] = field(
+        default=None,
+        metadata={
+            "name": "CurrencyRef",
+            "type": "Element",
+        },
+    )
+    exchange_rate: Optional[float] = field(
+        default=None,
+        metadata={
+            "name": "ExchangeRate",
+            "type": "Element",
+        },
+    )
+    amount_in_home_currency: Optional[Decimal] = field(
+        default=None,
+        metadata={
+            "name": "AmountInHomeCurrency",
+            "type": "Element",
+        },
+    )
+    ref_number: Optional[str] = field(
+        default=None,
+        metadata={
+            "name": "RefNumber",
+            "type": "Element",
+            "max_length": 11,
+        },
+    )
+    memo: Optional[str] = field(
+        default=None,
+        metadata={
+            "name": "Memo",
+            "type": "Element",
+            "max_length": 4095,
+        },
+    )
+    external_guid: Optional[str] = field(
+        default=None,
+        metadata={
+            "name": "ExternalGUID",
+            "type": "Element",
+        },
+    )
+    expense_line_ret: List[ExpenseLine] = field(
+        default_factory=list,
+        metadata={
+            "name": "ExpenseLineRet",
+            "type": "Element",
+        },
+    )
+    item_line_ret: List[ItemLine] = field(
+        default_factory=list,
+        metadata={
+            "name": "ItemLineRet",
+            "type": "Element",
+        },
+    )
+    item_group_line_ret: List[ItemGroupLine] = field(
+        default_factory=list,
+        metadata={
+            "name": "ItemGroupLineRet",
+            "type": "Element",
+        },
+    )
+    data_ext_ret: List[DataExt] = field(
+        default_factory=list,
+        metadata={
+            "name": "DataExtRet",
+            "type": "Element",
+        },
+    )
+
+
+class CreditCardCredits(PluralMixin, PluralTrxnSaveMixin):
+
+    class Meta:
+        name = "CreditCardCredit"
+        plural_of = CreditCardCredit
