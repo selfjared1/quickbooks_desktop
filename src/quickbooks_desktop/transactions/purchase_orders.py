f77b0f695c044b69142655eb5669fec8b518f9dd
Jared Self <self.jared@gmail.com>
2024-10-31 15:05:14 -0600

    Refactor Purchase Order imports
    Cleaned up and consolidated import statements in purchase_orders.py for better readability and maintenance. Removed redundant imports and grouped related modules to streamline the import section.

--- src/quickbooks_desktop/transactions/purchase_orders.py
+++ src/quickbooks_desktop/transactions/purchase_orders.py
@@ -5,11 +5,8 @@ from src.quickbooks_desktop.data_ext import DataExt
 
 from src.quickbooks_desktop.qb_special_fields import QBDates, QBTime
 from src.quickbooks_desktop.common import (
-    ExpenseLineMod, VendorAddress, VendorAddressBlock,
-    ModifiedDateRangeFilter, TxnDateRangeFilter, EntityFilter, AccountFilter, RefNumberFilter, RefNumberRangeFilter,
-    CurrencyFilter, VendorAddress, ShipAddress, ShipAddressBlock,
-    ItemLineAdd, ExpenseLineAdd, ItemGroupLineAdd, ItemLineMod, ItemGroupLineMod, LinkedTxn, ExpenseLine,
-    ItemLine, ItemGroupLine
+    VendorAddressBlock, ModifiedDateRangeFilter, TxnDateRangeFilter, EntityFilter, AccountFilter, RefNumberFilter, RefNumberRangeFilter,
+    CurrencyFilter, VendorAddress, ShipAddress, ShipAddressBlock, LinkedTxn,
 )
 from src.quickbooks_desktop.lists import (
     VendorRef, ClassInQBRef, InventorySiteRef, ItemRef, InventorySiteLocationRef,
