4747eb25c9f3686701e01bf52fd0cd5ca35f246a
Jared Self <self.jared@gmail.com>
2024-10-31 15:36:19 -0600

    Simplify imports in receive_payments.py
    Removed unnecessary imports and streamlined the essential ones. This cleanup enhances code readability and reduces potential maintenance overhead.

--- src/quickbooks_desktop/transactions/receive_payments.py
+++ src/quickbooks_desktop/transactions/receive_payments.py
@@ -1,23 +1,20 @@
 
 from dataclasses import dataclass, field
 from decimal import Decimal
-from typing import Optional, List, Type, Dict
-from collections import defaultdict
+from typing import Optional, List, Type
 from src.quickbooks_desktop.data_ext import DataExt
 from src.quickbooks_desktop.qb_special_fields import QBDates, QBTime
 from src.quickbooks_desktop.common import (
-    LinkedTxn, BillAddress, BillAddressBlock, ShipAddress, ShipAddressBlock, SetCredit,
     ModifiedDateRangeFilter, TxnDateRangeFilter, EntityFilter, AccountFilter, RefNumberFilter, RefNumberRangeFilter,
     CurrencyFilter, CreditCardTxnInfo, AppliedToTxn, AppliedToTxnAdd, AppliedToTxnMod
 )
 from src.quickbooks_desktop.lists import (
-    SalesTaxCodeRef, ItemSalesTaxRef, TemplateRef, CustomerRef, ClassInQBRef, AraccountRef, CurrencyRef, TermsRef,
-    SalesRepRef, ShipMethodRef, CustomerMsgRef, CustomerSalesTaxCodeRef, ItemRef, OverrideUomsetRef, InventorySiteRef,
-    InventorySiteLocationRef, ItemGroupRef, PriceLevelRef, OverrideItemAccountRef, AccountRef, PaymentMethodRef, DepositToAccountRef
+    CustomerRef, AraccountRef, CurrencyRef,
+    PaymentMethodRef, DepositToAccountRef
 )
 from src.quickbooks_desktop.mixins import (
-    PluralMixin, PluralTrxnSaveMixin, QBRefMixin, QBMixinWithQuery, QBMixin,
-    QBQueryMixin, QBAddMixin, QBModMixin, SaveMixin
+    PluralMixin, PluralTrxnSaveMixin, QBMixinWithQuery, QBMixin,
+    QBQueryMixin, QBAddMixin, QBModMixin,
 )
 
 
